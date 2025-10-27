287df7b64e70df93b7648597074e03bb5a05b7f0
Miguel Guerrero <73140853+Miguel565@users.noreply.github.com>
2025-10-27 21:58:08 +0000

    8.15: Database, step 3

--- models/authors.js
+++ models/authors.js
@@ -12,4 +12,12 @@ const authorSchema = new mongoose.Schema({
   },
 })
 
+authorSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
 module.exports = mongoose.model('Author', authorSchema)
